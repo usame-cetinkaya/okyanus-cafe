f5883ac4dc17754e5e74b9119633ee1251e12d99
Üsame Çetinkaya <142397067+usame-cetinkaya@users.noreply.github.com>
2025-11-23 19:12:52 +0300

    add kid list to admin dashboard

--- src/pages/admin-dashboard.tsx
+++ src/pages/admin-dashboard.tsx
@@ -1,5 +1,39 @@
+import { DataTable } from "@/tables/users/data-table.tsx";
+import { columns as sessionColumns } from "@/tables/sessions/columns.tsx";
+import { columns as kidsColumns } from "@/tables/kids/columns.tsx";
+import { useEffect, useState } from "react";
+import type { Kid, Session } from "@/lib/models.ts";
+import { pb } from "@/lib/pocketbase.ts";
+
 function AdminDashboard() {
-  return <h1>Oyun Alanı</h1>;
+  const [sessions, setSessions] = useState<Session[]>([]);
+  const [kids, setKids] = useState<Kid[]>([]);
+
+  useEffect(() => {
+    pb.collection("sessions")
+      .getFullList<Session>({
+        expand: "kid",
+        sort: "-start",
+        filter: 'end=""',
+      })
+      .then((sessions) => {
+        setSessions(sessions);
+      });
+
+    pb.collection("kids")
+      .getFullList<Kid>({ expand: "parent" })
+      .then((kids: Kid[]) => {
+        setKids(kids);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-6">
+      <DataTable columns={sessionColumns} data={sessions} />
+      <h2 className="text-xl">Çocuklar</h2>
+      <DataTable columns={kidsColumns} data={kids} />
+    </div>
+  );
 }
 
 export default AdminDashboard;
